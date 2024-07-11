cf4ee60ba1914411c0db880ec9a6b1a58c694a7b
mhkna <michkna3@gmail.com>
2024-07-11 00:11:52 -0400

    add way to stop game

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        if guess == "quit\n" {
+            println!("You quit the game :(");
+            break;
+        }
+
         //let guess: u32 = guess.trim().parse().expect("Please type a number!");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
